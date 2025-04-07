ca8fd264539e51baf53a4e40dab364838ae0039f
Anh Huynh <hma.tech04@gmail.com>
2025-04-07 15:54:29 +0700

    update call back api of Vnpay

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b129859e65cac1c6a1d03bfc9d3975d9c8f1189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785af2d5cb4ade1305d0442b9130659edde5d983")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
